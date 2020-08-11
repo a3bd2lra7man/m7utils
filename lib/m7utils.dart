9634c98a818be1643226df3fd8af02a5c9d29ceb
a3bd2lra7man <a3bd2llah@gmail.com>
2020-08-11 17:17:53 +0200

    License

--- lib/m7utils.dart
+++ lib/m7utils.dart
@@ -7,7 +7,9 @@ import 'package:flutter/material.dart'
 import 'package:flutter/services.dart' show rootBundle;
 import 'dart:convert' show json;
 import 'package:provider/provider.dart' show Provider;
-
+import 'dart:io' show HttpClientRequest,HttpClient,X509Certificate,HttpHeaders,HttpClientResponse;
+import 'dart:convert' show json,utf8;
+part 'utils/M7HttpClient.dart';
 part 'utils/Extensions.dart';
 part 'utils/AppLocalization.dart';
 part 'widgets/CircleImage.dart';
---
+++ lib/utils/M7HttpClient.dart
@@ -0,0 +1,90 @@
+part of m7utils;
+
+enum RequestType{
+  getT,post,put
+}
+
+typedef HttpRequester = Future<HttpClientRequest> Function(Uri);
+Client _client;
+
+class Client{
+
+  String localhost ;
+  String _token;
+
+
+  HttpClient httpClient;
+
+  factory Client({String localhost}){
+    if(_client == null){
+      _client = Client._initial(localhost);
+    }
+    _client.localhost ??= localhost ;
+    return _client;
+  }
+
+
+  Client._initial(this.localhost){
+    this.httpClient = HttpClient()
+      ..badCertificateCallback = ((X509Certificate cert ,String host,int port)=>true);
+  }
+
+
+  dynamic _toJson(Map map) =>  utf8.encode(json.encode(map));
+
+
+  HttpRequester _checkRequestType(RequestType requestType){
+
+    HttpRequester request;
+    switch(requestType){
+      case RequestType.getT:
+        request =  this.httpClient.getUrl;
+        break;
+      case RequestType.post:
+        request = this.httpClient.postUrl;
+        break;
+      case RequestType.put:
+        request = this.httpClient.putUrl;
+        break;
+    }
+    return request;
+
+  }
+  Future request({RequestType requestType,String url,Map data,bool isSecure = false})async{
+
+    try {
+
+      HttpClientRequest request = await _checkRequestType(requestType)(Uri.parse("${this.localhost}/$url"),);
+      request.headers.add(HttpHeaders.contentTypeHeader, 'application/json');
+      if(isSecure) request.headers.add(HttpHeaders.authorizationHeader, "Bearer $_token");
+      if(data != null)  request.add(this._toJson(data));
+      return await _getResponse(request);
+
+    }catch(e){
+      print(e);
+      return this._errorMessage;
+
+    }
+  }
+
+
+  Future<Map> _getResponse(HttpClientRequest request )async{
+
+    HttpClientResponse response = await request.close();
+    String reply = await response.transform(utf8.decoder).join();
+    Map res = json.decode(reply);
+    print("response from server : -------------------------------------------------------- \n $res");
+    return res;
+
+  }
+
+  Map _errorMessage = {
+    "resultCode" : 0,
+    "errorEnMessage" : "there is no internet",
+    "errorArMessage" : "لا يوجد اتصال بالانترنت"
+  };
+
+  void setToken(String token){
+    _token = token;
+  }
+}
