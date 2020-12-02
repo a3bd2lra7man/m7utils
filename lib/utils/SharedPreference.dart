623a7a1f8866bee2830b431889b6fffd58cbc29b
a3bd2lra7man <a3bd2llah@gmail.com>
2020-12-02 07:54:20 +0200

    License

--- lib/utils/SharedPreference.dart
+++ lib/utils/SharedPreference.dart
@@ -1,8 +1,9 @@
 part of m7utils;
 
+class M7Preferences{
+  Future<SharedPreferences> getSharedPreference()async => await SharedPreferences.getInstance();
 
-Future<SharedPreferences> getSharedPreference()async => await SharedPreferences.getInstance();
+  Future<String> getStringFromSharedPreferences(String key) async => (await getSharedPreference()).getString(key);
 
-Future<String> getStringFromSharedPreferences(String key) async => (await getSharedPreference()).getString(key);
-
-Future<bool> setStringInSharedPreference(String key , String value) async => (await getSharedPreference()).setString(key, value);
+  Future<bool> setStringInSharedPreference(String key , String value) async => (await getSharedPreference()).setString(key, value);
+}
