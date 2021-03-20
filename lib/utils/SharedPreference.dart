b65f682e94b51fbaa53d2445a172cb1edf3b6db4
newmac <newmac@MacBook-Pro.local>
2021-03-20 17:29:30 +0200

    bug fixes

--- lib/utils/SharedPreference.dart
+++ lib/utils/SharedPreference.dart
@@ -1,9 +1,12 @@
 part of m7utils;
 
-class M7Preferences{
-  Future<SharedPreferences> getSharedPreference()async => await SharedPreferences.getInstance();
+class M7Preferences {
+  Future<SharedPreferences> getSharedPreference() async =>
+      await SharedPreferences.getInstance();
 
-  Future<String> getStringFromSharedPreferences(String key) async => (await getSharedPreference()).getString(key);
+  Future<String?> getStringFromSharedPreferences(String key) async =>
+      (await getSharedPreference()).getString(key);
 
-  Future<bool> setStringInSharedPreference(String key , String value) async => (await getSharedPreference()).setString(key, value);
+  Future<bool> setStringInSharedPreference(String key, String value) async =>
+      (await getSharedPreference()).setString(key, value);
 }
